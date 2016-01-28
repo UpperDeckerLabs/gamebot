e30a26375b03f67a9fa0ae9eb06f73bdeb8f1cf1
Adam Weeks <me@adamweeks.com>
2016-01-27 22:34:44 -0500

    21 game moved to module.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var token = require('./token');
 var Botkit = require('botkit');
+var game21 = require('./game.21');
 
 var controller = Botkit.slackbot({
     logLevel: 1
@@ -14,48 +15,5 @@ controller.spawn({
 });
 
 controller.hears('^21', ['direct_mention','mention'], function(bot, message) {
-    bot.startConversation(message, dealHand);
+    game21.newGame(bot, message);
 });
-
-function dealHand(response, convo) {
-    var hand = 'J 4';
-    takeTurn(hand, response, convo);
-    convo.next();
-}
-
-function hitCard(hand, response, convo) {
-    convo.say('hitting');
-    hand += ' 5';
-    takeTurn(hand, response, convo);
-    convo.next();
-}
-
-function stay(hand, response, convo) {
-    convo.say('staying');
-    convo.next();
-}
-
-function takeTurn(hand, response, convo) {
-    convo.say('Your hand: ' + hand);
-    convo.ask('Would you like to "(H)IT or (S)TAY"?', [
-        {
-            pattern: /^(h|H|hit|Hit|HIT)/i,
-            callback: function(response, convo) {
-                hitCard(hand, response, convo);
-            }
-        },
-        {
-            pattern: /^(s|S|stay|Stay|STAY)/i,
-            callback: function(response, convo) {
-                stay(hand, response, convo);
-            }
-        },
-        {
-            default: true,
-            callback: function(response, convo) {
-                convo.repeat();
-                convo.next();
-            }
-        }
-    ]);
-}
