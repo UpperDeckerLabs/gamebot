dff25c9149891aba01c83194f83505a877c1718b
Adam Weeks <me@adamweeks.com>
2016-02-28 13:57:51 -0500

    Support for /version endpoint

--- app/server/index.js
+++ app/server/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var stats = require('./stats');
+var pjson = require('../../package.json');
 
 var server = {
     run: run
@@ -12,13 +13,17 @@ function run(port, storage) {
     app.set('view engine', 'jade');
 
     app.get('/', function (req, res) {
-        res.send('Gamebot is running!');
+        res.status(200).send('Gamebot is running!');
     });
 
     app.get('/stats', function(req, res) {
         var allUsers = stats.all(storage);
         res.render('stats', { users: allUsers });
     });
+
+    app.get('/version', function(req, res) {
+        res.status(200).send('Current version: ' + pjson.version);
+    })
 
     app.listen(port, function() {
       console.log('Server listening on port: '  + port);
